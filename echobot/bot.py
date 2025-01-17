b186104b528d888c0b9a52e56c13814e23917c50
Maria Rosa <mjordang@gmail.com>
2025-01-17 12:58:26 +0100

    feat: add logic to send events from back

--- echobot/bot.py
+++ echobot/bot.py
@@ -1,4 +1,5 @@
 from botbuilder.core import ActivityHandler, MessageFactory, TurnContext
+from botbuilder.schema import ActivityTypes, Activity
 from botbuilder.schema import ChannelAccount
 
 
@@ -6,11 +7,22 @@ class EchoBot(ActivityHandler):
     async def on_members_added_activity(
         self, members_added: [ChannelAccount], turn_context: TurnContext
     ):
+        print("on_members_added_activity called")
         for member in members_added:
             if member.id != turn_context.activity.recipient.id:
                 await turn_context.send_activity("Hello and welcome!")
 
     async def on_message_activity(self, turn_context: TurnContext):
+        print(f"on_message_activity called: {turn_context.activity.text}")
+        if "view" in turn_context.activity.text.lower():
+            # TODO: Send event from back to front requesting an screenshot
+            event_activity = Activity(
+                type=ActivityTypes.event,
+                name="makeScreenshot",
+                value={"key": "value"}
+            )
+            await turn_context.send_activity(event_activity)
+
         return await turn_context.send_activity(
             MessageFactory.text(f"Echo: {turn_context.activity.text}")
         )
